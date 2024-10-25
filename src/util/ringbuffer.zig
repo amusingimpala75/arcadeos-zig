1fa73e7cb857c897a9c2678cd2c00a58d21f736d
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2024-10-24 23:51:26 -0400

    Make the Ringbuffer a proper Ringbuffer

--- src/util/ringbuffer.zig
+++ src/util/ringbuffer.zig
@@ -2,29 +2,32 @@ const std = @import("std");
 
 const kernel = @import("../kernel.zig");
 
-pub fn Ringbuffer(comptime T: type, comptime capacity: usize, comptime empty: T) type {
+pub fn Ringbuffer(comptime T: type, comptime capacity: usize) type {
     return struct {
-        arr: [capacity]T,
-        head: usize,
+        arr: [capacity]T = .{undefined} ** capacity,
+        front: usize = 0, // next space to be filled by enqueue
+        back: usize = 0, // next item returned by dequeue
+        size: usize = 0,
 
-        pub fn init() @This() {
-            return .{
-                .arr = [1]T{empty} ** capacity,
-                .head = 0,
-            };
+        const RB = @This();
+
+        pub fn enqueue(self: *RB, val: T) !void {
+            if (self.size == self.arr.len) {
+                return error.BufferFull;
+            }
+            self.arr[self.front] = val;
+            self.front = (self.front + 1) % capacity;
+            self.size += 1;
         }
 
-        pub fn len(self: @This()) usize {
-            return self.arr.len;
-        }
-
-        pub fn advanceHead(self: *@This()) void {
-            self.head = (self.head + 1) % self.arr.len;
-        }
-
-        pub fn get(self: *@This(), index: usize) *T {
-            const actual_index = ((self.head + self.arr.len) - index) % self.arr.len;
-            return &self.arr[actual_index];
+        pub fn dequeue(self: *RB) ?T {
+            if (self.size == 0) {
+                return null;
+            }
+            const ptr: *T = &self.arr[self.back];
+            self.back = (self.back + 1) % capacity;
+            self.size -= 1;
+            return ptr.*;
         }
     };
 }
